6adf523cfbc1edb19bb40ab3f5a3e2253bd9f1ca
Stacy Kim <hgzxbprn@gmail.com>
2020-11-10 01:54:12 +0000

    Can now toggle abstracts to show/hide.

--- js/custom.js
+++ js/custom.js
@@ -80,3 +80,15 @@
     });
 
 })(jQuery);
+
+function showAbstract(id_abs,id_button) {
+    var abs = document.getElementById(id_abs);
+    var button = document.getElementById(id_button);
+  if (abs.style.display === "none") {
+      abs.style.display = "block";
+      button.firstChild.data = "\u2796";
+  } else {
+      abs.style.display = "none";
+      button.firstChild.data = "\u2795";
+  }
+}
